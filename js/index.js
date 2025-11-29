029d44fd2c409a8424b9071fe3534d1e7d2a1c74
Cristian <cristian.cm390@gmail.com>
2025-11-28 22:03:21 -0300

    commit 2

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function changeMainImage(imageSrc) {
+    document.getElementById('main-image').src = imageSrc;
+}
+
+function changeMainImage2(imageSrc) {
+    document.getElementById('main-image2').src = imageSrc;
+}
